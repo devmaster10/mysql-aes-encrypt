11edc5d17dc14ade26d5e11c0fb94b469345ec29
Marcelo Barros <mbarros@livesponsors.com>
2018-05-18 17:12:20 +0100

    Mysql encrypt

--- src/helpers.php
+++ src/helpers.php
@@ -2,17 +2,28 @@
 
 namespace AESEncrypt;
 
-if (!function_exists('AESEncrypt\decrypt')) {
+if (!function_exists('AESEncrypt\mysqldecrypt')) {
     /**
      * Creates a Stringy object and returns it on success.
      *
      * @param  string  $column The character encoding
      * @return Stringy column with decrypt function
      */
-    function decrypt($column, $encoding = null)
+    function mysqldecrypt($column)
     {
-        // return "AES_DECRYPT({$column}, '" . env('APP_AESENCRYPT_KEY') ."')";
+        return "AES_DECRYPT({$column}, '" . env('APP_AESENCRYPT_KEY') ."')";
+    }
+}
 
-        return "AES_DECRYPT({$column}, 'TESTE')";
+if (!function_exists('AESEncrypt\mysqlencrypt')) {
+    /**
+     * Creates a Stringy object and returns it on success.
+     *
+     * @param  string  $column The character encoding
+     * @return Stringy column with decrypt function
+     */
+    function mysqlencrypt($column)
+    {
+        return "AES_ENCRYPT({$column}, '" . env('APP_AESENCRYPT_KEY') ."')";
     }
 }
